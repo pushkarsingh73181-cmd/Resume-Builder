7be3e15c61b7e1a253d8a6c5ce0174683e7d0ca6
pushkarsingh73181-cmd <pushkarsingh73181@gmail.com>
2026-05-23 16:43:45 +0530

    Services Done

---
+++ services/claudeService.js
@@ -0,0 +1,10 @@
+// Placeholder for Claude AI service - Week 2
+export const generateResumeQuestions = async (jd) => {
+  // To be implemented in Week 2
+  throw new Error('Not implemented yet')
+}
+
+export const analyzeJD = async (jd) => {
+  // To be implemented in Week 2
+  throw new Error('Not implemented yet')
+}
