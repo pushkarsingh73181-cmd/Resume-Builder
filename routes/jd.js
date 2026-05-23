96d67e8ee58eaa421f8be97c70ae253a0e4a6fee
pushkarsingh73181-cmd <pushkarsingh73181@gmail.com>
2026-05-23 20:39:50 +0530

    Delete routes directory

--- routes/jd.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.post('/analyze', (req, res) => {
-  res.status(501).json({ error: 'JD analysis coming in Week 2' })
-})
-
-export default router
